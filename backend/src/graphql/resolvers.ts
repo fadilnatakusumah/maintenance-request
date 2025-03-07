8632f9ce1bab92726d241c98ac3a0cf342bb6b26
fadilnatakusumah <fadil.ntksmh@gmail.com>
2025-03-07 22:34:46 +0700

    feat: done for the CreateMaintenanceRequest

--- backend/src/graphql/resolvers.ts
+++ backend/src/graphql/resolvers.ts
@@ -1,10 +1,27 @@
+import { prisma } from "../prisma";
+import { CreateMaintenanceRequestInput } from "../__generated__/graphql";
+
 export const resolvers = {
   Query: {
     maintenanceRequests: async () => {
-      return [];
+      return prisma.maintenanceRequest.findMany();
     },
   },
 
-  Mutation: {},
+  Mutation: {
+    createMaintenanceRequest: async (
+      _: unknown,
+      { input }: { input: CreateMaintenanceRequestInput }
+    ) => {
+      return prisma.maintenanceRequest.create({
+        data: {
+          title: input.title,
+          status: input.status,
+          description: input.description,
+          urgency: input.urgency,
+        },
+      });
+    },
+  },
   Subscription: {},
 };
